698f3f6534f9f99a2c4a118de8248211eb28fda0
Casto Mlingi <macbookpro@MacBooks-MacBook-Pro.local>
2026-02-05 13:02:26 +0300

    fix: Temporarily revert to Tailwind CDN to restore UI styling while fixing PostCSS setup

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import './index.css';
+// import './index.css'; // Temporarily disabled - using CDN for now
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
